cefbd796610f84adcf44e1f992279f2e0cd33e54
archord <xyag.902@163.com>
2019-01-28 18:50:26 +0800

    增加上传多媒体文件的功能，mediaType媒体文件类型，分别有图片（image）、语音（voice）、视频（video），普通文件（file）。

--- gwac-web-backend/src/main/java/com/gwac/action/SendTriggerToWChart.java
+++ gwac-web-backend/src/main/java/com/gwac/action/SendTriggerToWChart.java
@@ -10,9 +10,12 @@ package com.gwac.action;
  */
 import com.gwac.dao.WebGlobalParameterDao;
 import com.opensymphony.xwork2.ActionSupport;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletResponse;
 import me.chanjar.weixin.common.error.WxErrorException;
@@ -38,15 +41,19 @@ public class SendTriggerToWChart extends ActionSupport {
   private String triggerMsg;
   private String chatId;
 
+  private String mediaType; //媒体文件类型，分别有图片（image）、语音（voice）、视频（video），普通文件（file）
+  private List<File> fileUpload = new ArrayList<>();
+  private List<String> fileUploadContentType = new ArrayList<>();
+  private List<String> fileUploadFileName = new ArrayList<>();
+
   private String echo = "";
 
   @Action(value = "sendTrigger2WChart")
   public String upload() {
 
     echo = "";
-
-    if (triggerMsg == null || triggerMsg.isEmpty()) {
-      echo = "triggerMsg is empty, please check.";
+    if ((triggerMsg == null || triggerMsg.isEmpty()) && (fileUpload.isEmpty())) {
+      echo = "triggerMsg and file is empty, please check.";
     } else {
 
       String wtag = "weixin1";
@@ -70,21 +77,42 @@ public class SendTriggerToWChart extends ActionSupport {
       WxCpServiceImpl wxCpService = new WxCpServiceImpl();
       wxCpService.setWxCpConfigStorage(config);
 
-      if(chatId==null || chatId.isEmpty()){
-        setChatId("gwac001");
+      if (chatId == null || chatId.isEmpty()) {
+	setChatId("gwac001");
       }
-      String url = "https://qyapi.weixin.qq.com/cgi-bin/appchat/send";
-      String msg = "{\"chatid\" : \""+chatId+"\",\"msgtype\" : \"text\",\"safe\" : \"0\",\"text\" : {\"content\": \"" + triggerMsg + "\"}}";
-      //String urlCreateQun = "https://qyapi.weixin.qq.com/cgi-bin/appchat/create";
-      //String msgCreateQun = "{\"name\" : \"GWAC001\",\"owner\" : \"XuYang\",\"userlist\" : [\"XuYang\", \"Long\", \"zheng_ya_tong\"],\"chatid\" : \"gwac001\"}";
-      //echo = wxCpService.post(urlCreateQun, msgCreateQun);
-
       String trst;
+      String url = "https://qyapi.weixin.qq.com/cgi-bin/appchat/send";
       try {
-        echo = wxCpService.post(url, msg);
-        log.debug(echo);
+	if ((triggerMsg != null) && (!triggerMsg.isEmpty())) {
+	  String msg = "{\"chatid\" : \"" + chatId + "\",\"msgtype\" : \"text\",\"safe\" : \"0\",\"text\" : {\"content\": \"" + triggerMsg + "\"}}";
+	  //String urlCreateQun = "https://qyapi.weixin.qq.com/cgi-bin/appchat/create";
+	  //String msgCreateQun = "{\"name\" : \"GWAC001\",\"owner\" : \"XuYang\",\"userlist\" : [\"XuYang\", \"Long\", \"zheng_ya_tong\"],\"chatid\" : \"gwac001\"}";
+	  //echo = wxCpService.post(urlCreateQun, msgCreateQun);
+
+	  echo = wxCpService.post(url, msg);
+	  log.debug(echo);
+	}
+
+	if (fileUpload.size() > 0) {
+
+	  if (mediaType == null || mediaType.isEmpty()) {
+	    mediaType="image";
+	  }
+	  for (File file : fileUpload) {
+	    String mediaId = wxCpService.getMediaService().upload(mediaType, file).getMediaId();
+	    if ((mediaId != null) && (!mediaId.isEmpty())) {
+	      String msg = "{\"chatid\" : \"" + chatId + "\",\"msgtype\" : \"" + mediaType + "\",\"safe\" : \"0\",\"" + mediaType + "\" : {\"media_id\": \"" + mediaId + "\"}}";
+	      echo = wxCpService.post(url, msg);
+	      log.debug(echo);
+	    } else {
+	      echo = "upload failure";
+	      log.error(echo);
+	    }
+	  }
+	}
+
       } catch (WxErrorException ex) {
-        log.error("send trigger to wechart error:", ex);
+	log.error("send trigger to wechart error:", ex);
       }
     }
     log.debug(echo);
@@ -125,5 +153,33 @@ public class SendTriggerToWChart extends ActionSupport {
    */
   public void setChatId(String chatId) {
     this.chatId = chatId;
+  }
+
+  /**
+   * @param mediaType the mediaType to set
+   */
+  public void setMediaType(String mediaType) {
+    this.mediaType = mediaType;
+  }
+
+  /**
+   * @param fileUpload the fileUpload to set
+   */
+  public void setFileUpload(List<File> fileUpload) {
+    this.fileUpload = fileUpload;
+  }
+
+  /**
+   * @param fileUploadContentType the fileUploadContentType to set
+   */
+  public void setFileUploadContentType(List<String> fileUploadContentType) {
+    this.fileUploadContentType = fileUploadContentType;
+  }
+
+  /**
+   * @param fileUploadFileName the fileUploadFileName to set
+   */
+  public void setFileUploadFileName(List<String> fileUploadFileName) {
+    this.fileUploadFileName = fileUploadFileName;
   }
 }
